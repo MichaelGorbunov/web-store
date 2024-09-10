da7eb3695f6182c4a358e93a7b8d36f90bdfb770
Michael Gorbunov <mg0rby@yandex.ru>
2024-09-10 15:31:20 +0300

    icon site

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# from django.urls import path
+from django.views.generic.base import RedirectView
 from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('students/', include('students.urls')),
     path('', include('students.urls')),
+    path("favicon.ico",RedirectView.as_view(url='/static/img/favicon.ico')),
 
 ]
+
