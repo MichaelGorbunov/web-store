390c28a31fd1afbed7a933c7cdec70304a42c66c
Michael Gorbunov <mg0rby@yandex.ru>
2024-09-10 10:46:13 +0300

    вывод данных с формы в консоль

--- students/views.py
+++ students/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 # students/views.py
@@ -7,5 +8,17 @@ from django.shortcuts import render
 def about(request):
     return render(request, 'students/about.html')
 
+# def contact(request):
+#     return render(request, 'students/contact.html')
+
 def contact(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+        # Здесь мы просто возвращаем простой ответ
+        print(name)
+        print(message)
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'students/contact.html')
