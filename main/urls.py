845ef1a6f3d83ee14c86f07312386259676536cc
Michael Gorbunov <mg0rby@yandex.ru>
2024-10-01 16:03:08 +0300

    main add app

--- main/urls.py
+++ main/urls.py
@@ -4,11 +4,12 @@ from django.contrib import admin
 from django.urls import path
 
 from main.views import contact, BlogListView, blog_item, IndexView
+app_name="main"
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', IndexView.as_view()),
-    path('contact/', contact),
-    path('blog/', BlogListView.as_view()),
+    path('', IndexView.as_view(),name="index"),
+    path('contact/', contact,name="contact"),
+    path('blog/', BlogListView.as_view(),name="blog"),
     path('blog/<int:pk>/', blog_item)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
